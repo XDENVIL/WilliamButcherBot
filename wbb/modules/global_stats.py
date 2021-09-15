f213ba96fa5bd0c9b0c184b218cd20888943d66f
DENVIL <84806367+XDENVIL@users.noreply.github.com>
2021-09-16 01:03:48 +0530

    Update global_stats.py

--- wbb/modules/global_stats.py
+++ wbb/modules/global_stats.py
@@ -103,7 +103,7 @@ async def global_stats(_, message):
     karmas_chats_count = _karmas["chats_count"]
 
     # Contributors/Developers count and commits on github
-    url = "https://api.github.com/repos/xdenvil"
+    url = "https://api.github.com/repos/thehamkercat/williambutcherbot/contributors"
     rurl = "https://github.com/xdenvil"
     developers = await get(url)
     commits = 0
